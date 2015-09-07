6352886b99b97b2f610add1b5ce5c17d59dd0cb5
Paul Panserrieu <paul.panserrieu@gmail.com>
2015-09-07 22:02:59 +0200

    wip on testing `tarifa prepare`

--- test/cli/prepare.js
+++ test/cli/prepare.js
@@ -1,4 +1,32 @@
 var test = require('tape'),
+    spawn = require('tape-spawn'),
     h = require('../helpers');
 
 test('tarifa prepare -h', h.usageTest('prepare'));
+
+test('tarifa platform add browser', function (t) {
+    var projVal = h.currentProjectVal(),
+        pwd = process.cwd();
+
+    process.chdir(projVal.tmpPath);
+    var st = spawn(t, h.cmd('platform add browser'));
+    st.exitCode(0);
+    process.chdir(pwd);
+    st.end();
+});
+
+test('tarifa prepare browser', function (t) {
+    var projVal = h.currentProjectVal(),
+        pwd = process.cwd();
+
+    process.chdir(projVal.tmpPath);
+    var st = spawn(t, h.cmd('prepare browser'));
+    st.exitCode(0);
+    process.chdir(pwd);
+    st.end();
+});
+
+test('tarifa prepare browser result', function (t) {
+    t.plan(1);
+    t.ok(h.isFile(h.currentProjectVal().tmpPath, 'project/www/main.js'), 'main.js created');
+});
