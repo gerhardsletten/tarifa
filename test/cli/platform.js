0b857c2256bd164ec95609de48db83e68dd3dc08
Paul Panserrieu <paul.panserrieu@gmail.com>
2015-09-09 12:04:11 +0200

    test tarifa prepare

--- test/cli/platform.js
+++ test/cli/platform.js
@@ -1,4 +1,24 @@
 var test = require('tape'),
+    spawn = require('tape-spawn'),
+    format = require('util').format,
     h = require('../helpers');
 
-test('tarifa platform -h', h.usageTest('platform'));
+test('cli: tarifa platform -h', h.usageTest('platform'));
+
+h.platforms().forEach(function (platform) {
+    test(format('cli: tarifa platform remove %s', platform), function (t) {
+        var st = spawn(t, h.cmd(format('platform remove %s', platform)));
+        st.exitCode(0);
+        st.end();
+    });
+    test(format('cli: tarifa platform add %s', platform), function (t) {
+        var st = spawn(t, h.cmd(format('platform add %s', platform)));
+        st.exitCode(0);
+        st.end();
+    });
+    test(format('cli: tarifa platform add %s', platform), function (t) {
+        var st = spawn(t, h.cmd(format('platform add %s', platform)));
+        st.stdout.match(/.*Platform already installed!.*/);
+        st.end();
+    });
+});
