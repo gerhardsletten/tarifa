e782e3e3b2c8cb060ef0257569de6d85f4fa020a
Paul Panserrieu <paul.panserrieu@gmail.com>
2015-09-09 16:04:32 +0200

    test tarifa plugin

--- test/cli/plugin.js
+++ test/cli/plugin.js
@@ -1,4 +1,66 @@
 var test = require('tape'),
-    h = require('../helpers');
+    spawn = require('tape-spawn'),
+    format = require('util').format,
+    h = require('../helpers'),
+    plugins = require('../../lib/plugins.json'),
+    tmp = h.currentPluginVal();
 
 test('cli: tarifa plugin -h', h.usageTest('plugin'));
+
+test(format('cli: tarifa plugin add %s', tmp.tmpPath), function (t) {
+    var st = spawn(t, h.cmd(format('plugin add %s', tmp.tmpPath)));
+    st.exitCode(0);
+    st.end();
+});
+
+test('cli: tarifa plugin list', function (t) {
+    var st = spawn(t, h.cmd('plugin list'));
+    st.stdout.match(new RegExp('.*' + tmp.id + '.*'));
+    st.exitCode(0);
+    st.end();
+});
+
+test('cli: tarifa plugin add https://github.com/apache/cordova-plugin-vibration.git#r0.3.11', function (t) {
+    var st = spawn(t, h.cmd('plugin add https://github.com/apache/cordova-plugin-vibration.git#r0.3.11'));
+    st.exitCode(0);
+    st.end();
+});
+
+test('cli: tarifa plugin list', function (t) {
+    var st = spawn(t, h.cmd('plugin list'));
+    st.stdout.match(/org\.apache\.cordova\.vibration/, 'added plugin matched');
+    st.exitCode(0);
+    st.end();
+});
+
+test('cli: tarifa plugin remove org.apache.cordova.vibration', function (t) {
+    var st = spawn(t, h.cmd('plugin remove org.apache.cordova.vibration'));
+    st.exitCode(0);
+    st.end();
+});
+
+test('cli: tarifa plugin remove cordova-plugin-splashscreen', function (t) {
+    var st = spawn(t, h.cmd('plugin remove cordova-plugin-splashscreen'));
+    st.exitCode(0);
+    st.end();
+});
+
+test('cli: tarifa plugin remove cordova-plugin-whitelist', function (t) {
+    var st = spawn(t, h.cmd('plugin remove cordova-plugin-whitelist'));
+    st.exitCode(0);
+    st.end();
+});
+
+test(format('cli: tarifa plugin remove %s', tmp.id), function (t) {
+    var st = spawn(t, h.cmd(format('plugin remove %s', tmp.id)));
+    st.exitCode(0);
+    st.end();
+});
+
+plugins.forEach(function (plugin) {
+    test(format('cli: tarifa plugin add %s', plugin.uri), function (t) {
+        var st = spawn(t, h.cmd(format('plugin add %s', plugin.uri)));
+        st.exitCode(0);
+        st.end();
+    });
+});
