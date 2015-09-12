da099ce7c3719ae09f9f4a96472705357026b5e0
Paul Panserrieu <paul.panserrieu@gmail.com>
2015-09-12 07:59:29 +0200

    test tarifa build

--- test/cli/build.js
+++ test/cli/build.js
@@ -1,4 +1,31 @@
 var test = require('tape'),
+    spawn = require('tape-spawn'),
+    format = require('util').format,
     h = require('../helpers');
 
 test('cli: tarifa build -h', h.usageTest('build'));
+
+test('cli: tarifa build all dev,stage', function (t) {
+    var st = spawn(t, h.cmd('build browser dev,stage'));
+    st.succeeds();
+    st.end();
+});
+
+h.platforms().forEach(function (platform) {
+    test(format('cli: tarifa build %s', platform), function (t) {
+        var st = spawn(t, h.cmd(format('build %s --verbose', platform)), {
+            stdio: 'inherit'
+        });
+        st.succeeds();
+        st.end();
+    });
+});
+
+test('cli: tarifa build browser in `project` folder', function (t) {
+    process.chdir('./project');
+    var st = spawn(t, h.cmd('build browser dev,stage'));
+    process.chdir('..');
+    st.succeeds();
+    st.end();
+});
+
