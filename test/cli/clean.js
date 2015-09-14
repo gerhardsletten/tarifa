382a65afdd88fcaa5782b8d55217187440d963c1
Paul Panserrieu <paul.panserrieu@gmail.com>
2015-09-14 17:01:42 +0200

    test tarifa clean

--- test/cli/clean.js
+++ test/cli/clean.js
@@ -1,4 +1,20 @@
 var test = require('tape'),
+    spawn = require('tape-spawn'),
+    format = require('util').format,
     h = require('../helpers');
+
+h.platforms().forEach(function (platform) {
+    test(format('cli: tarifa clean %s', platform), function (t) {
+        var st = spawn(t, h.cmd(format('clean %s --verbose', platform)));
+        st.succeeds();
+        st.end();
+    });
+});
+
+test('cli: tarifa clean', function (t) {
+        var st = spawn(t, h.cmd('clean --verbose'));
+        st.succeeds();
+        st.end();
+    });
 
 test('cli: tarifa clean -h', h.usageTest('clean'));
