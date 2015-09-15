789e0062c7b9d7c67ce320764280dff3d53e50e1
Paul Panserrieu <paul.panserrieu@gmail.com>
2015-09-15 13:25:25 +0200

    tarifa device minimal test

--- test/cli/device.js
+++ test/cli/device.js
@@ -1,4 +1,21 @@
 var test = require('tape'),
+    spawn = require('tape-spawn'),
     h = require('../helpers');
+
+test('cli: tarifa device', function (t) {
+    var st = spawn(t, h.cmd('device'), {
+        stdio: 'inherit'
+    });
+    st.succeeds();
+    st.end();
+});
+
+test('cli: tarifa device --verbose', function (t) {
+    var st = spawn(t, h.cmd('device --verbose'), {
+        stdio: 'inherit'
+    });
+    st.succeeds();
+    st.end();
+});
 
 test('cli: tarifa device -h', h.usageTest('device'));
